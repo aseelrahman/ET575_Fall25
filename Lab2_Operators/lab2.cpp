f4b22c8f1ff87c3d7bff4fc54fc87c0d16520c2e
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-09-03 19:53:47 +0000

    finish class example

--- Lab2_Operators/lab2.cpp
+++ Lab2_Operators/lab2.cpp
@@ -53,5 +53,30 @@ int main(){
     cout<<"is number1 greater than number2? "<<(number1 > number2)<< endl;
     cout<<"is number1 less than number2? "<<(number1 < number2)<< endl;
     cout<<"is number1 not equal to number2? "<<(number1 != number2)<< endl;
+
+    cout<<"\n------ example 4: logical operator ------ "<<endl;
+    // AND operator
+    bool check_numbers = (number1 != number2) && (number1 < number2) && (number1 > 5);
+    // number1 != number2 --> true
+    // number1 < number2 --> true
+    // number1>5 --> false
+    // check_numbers = true && true && false = false (0)
+    cout<<"The result of check_number is "<<check_numbers<<endl;
+
+    // OR operator
+    bool check_or = (number2 > 10) || (number2 != 3) || (number2 < number1);
+    // number2 > 10 --> false
+    // number2 != 3 --> true
+    // number2 < number1 --> false
+    // false || true || false = true (1)
+    cout<<"The result of check_or is "<<check_or<<endl;
+
+    cout<<"\n------ example 4: casting ------ "<<endl;
+    int n = 5;
+    float m = n/2;
+
+    cout<<"implicit casting = "<<m<<endl;
+
+    cout<<"\n ------ EXERCISE ------"<<endl;
     return 0;
 }
