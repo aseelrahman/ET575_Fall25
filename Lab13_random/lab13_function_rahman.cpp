657afac5cf62b70ff8b7e6ece7cc86fa28173cf1
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-10-27 19:29:05 +0000

    Lab13 complete

---
+++ Lab13_random/lab13_function_rahman.cpp
@@ -0,0 +1,58 @@
+/*
+Aseel Rahman
+Oct 27, 2025
+lab13: random variables
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+
+using namespace std;
+
+//function to return a random number
+int random_number(){
+    return rand();
+}
+
+// example 2: different random number
+int different_random(){
+    return rand();
+}
+
+// example 3: range of random number
+// generate random numbers between 0 and 9
+int random_0_9(){
+   return rand()%10; 
+}
+
+//example 4: specific range of random number
+int random_neg10_5(){
+    return -10 + rand()%16;
+}
+
+//Exercise
+// function that generates a random number between 1 and 6
+int random_1_6(){
+    return 1 + rand()%6;
+}
+
+//function to compare if two numbers are 1s
+bool compare_ones(int r1, int r2){
+    if(r1 == 1 && r2 == 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// function to prompt result
+void printresult(bool result){
+    if(result==true){
+        cout<<"congrats! You got a snake eyes"<<endl;
+    }
+    else{
+        cout<<"Better luck next time"<<endl;
+    }
+}
---
+++ Lab13_random/lab13_main_lastname.cpp
@@ -0,0 +1,43 @@
+/*
+Aseel Rahman
+Oct 27, 2025
+lab13: random variables
+*/
+
+#include<iostream>
+#include "lab13_function_rahman.cpp"
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+
+
+    cout<<"\n ----- Example 1: random number ----- "<<endl;
+    int r = random_number();
+    cout<<"Random number = "<<r<<endl;
+
+    cout<<"\n ----- Example 2: different random number ----- "<<endl;
+    int diff_random = different_random();
+    cout<<"Different random number: "<<diff_random<<endl;
+
+    cout<<"\n ----- Example 3: random number between 0 and 9 ----- "<<endl;
+    int r_0_9 = random_0_9();
+    cout<<"Number between 0 and 9: \t"<<r_0_9<<endl;
+
+    cout<<"\n ----- Example 4: random number between -10 and 5 ----- "<<endl;
+    cout<<"Number between -10 and 5 \t"<<random_neg10_5()<<endl;
+
+    cout<<"\n ----- EXERCISE ----- "<<endl;
+    int run1 = random_1_6();
+    int run2 = random_1_6();
+
+    cout<<"RUN1: "<<run1<<" RUN2: "<<run2<<endl;
+
+    //call function to check for snake_eyes
+    bool result = compare_ones(run1, run2);
+
+    //call function to print result
+    printresult(result);
+    return 0;
+}
